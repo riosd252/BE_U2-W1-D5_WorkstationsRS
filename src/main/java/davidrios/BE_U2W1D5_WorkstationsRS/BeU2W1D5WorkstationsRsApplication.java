b095d9f39f4d19a5f50c7bf8a20bc2c27c579747
David A. Rios S. <64142938+riosd252@users.noreply.github.com>
2024-02-09 12:14:02 +0100

    Initial commit

---
+++ src/main/java/davidrios/BE_U2W1D5_WorkstationsRS/BeU2W1D5WorkstationsRsApplication.java
@@ -0,0 +1,13 @@
+package davidrios.BE_U2W1D5_WorkstationsRS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W1D5WorkstationsRsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W1D5WorkstationsRsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidrios/BE_U2W1D5_WorkstationsRS/BeU2W1D5WorkstationsRsApplicationTests.java
@@ -0,0 +1,13 @@
+package davidrios.BE_U2W1D5_WorkstationsRS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W1D5WorkstationsRsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
